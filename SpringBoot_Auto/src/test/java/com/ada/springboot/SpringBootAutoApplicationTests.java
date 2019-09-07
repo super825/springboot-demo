97ba3b1501b52ad9b7ce81d2693cca85b3311f09
super825 <106498381@qq.com>
2019-09-07 11:18:34 +0800

    create springboot demo project

---
+++ SpringBoot_Auto/src/test/java/com/ada/springboot/SpringBootAutoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ada.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAutoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ SpringBoot_Self/src/main/java/com/ada/springboot/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.ada.springboot.service;
+
+import com.ada.springboot.beans.Employee;
+
+public interface EmployeeService {
+
+    Employee getEmployee();
+}
